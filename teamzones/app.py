241702bd0a8d862b16453c8aca1ff102d10f0b0e
Paul Chiu <paulchiu@gmail.com>
2025-01-25 10:14:51 +1000

    feat(list_countries): Created command

--- teamzones/app.py
+++ teamzones/app.py
@@ -1,7 +1,17 @@
 import typer
+from zoneinfo import ZoneInfo, available_timezones
 
 app = typer.Typer()
 
 @app.command()
-def hello(name: str):
+def main(name: str):
     typer.echo(f"Hello {name}!")
+
+@app.command()
+def list_countries():
+    timezones = sorted(available_timezones())
+    for timezone in timezones:
+        print(timezone)
+
+if __name__ == "__main__":
+    typer.run(main)
